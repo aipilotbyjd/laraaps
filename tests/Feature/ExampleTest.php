2781fc27a4d817cb19d1e0a1b7e7b9f59eb1fde5
Jaydeep Dhrangiya <jaydeepdhrangiya@Jaydeeps-MacBook-Air.local>
2025-10-18 19:58:40 +0530

    initial commit

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,17 +2,19 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use WithoutMiddleware;
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/v1/admin/system/health');
 
         $response->assertStatus(200);
     }
